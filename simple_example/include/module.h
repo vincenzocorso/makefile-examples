09431e2e326834ee7178197649e36879fb761632
vincenzocorso <vincenzocorso99@gmail.com>
2019-02-24 09:45:27 +0100

    Added a new example

---
+++ simple_example/include/module.h
@@ -0,0 +1,6 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+void print_hello();
+
+#endif /* MODULE_H */
---
+++ simple_example/src/module.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "module.h"
+
+void print_hello()
+{
+	puts("hello");
+}
